4d183462d3066586d7d18508e2f783d4efd4780e
aa4863586 <xie_w@isumeng.com>
2014-07-09 10:06:10 +0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,31 @@
+source 'https://rubygems.org'
+ruby '1.9.3'
+
+
+gem 'rails', '4.0.4'
+group :'development' do
+
+gem 'sqlite3'
+end
+
+
+gem 'sass-rails', '4.0.2'
+
+gem 'uglifier', '1.3.0'
+
+gem 'coffee-rails', '4.0.0'
+
+gem 'jquery-rails'
+
+gem 'turbolinks'
+
+gem 'jbuilder', '1.2'
+
+group :doc do
+  
+  gem 'sdoc', require: false
+end
+
+group :production do
+	gem 'pg', '0.15.1'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+XieweiApp::Application.config.secret_key_base = '28854e5026b2830600b1074e0d46510805c19cb36d6df2c6d12e100b70914aa6cc0282c10d9d1d937a7455d9b592931f67b20c015a61e35fbb67b82dc0206aed'
